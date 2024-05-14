cfdd7f038c0cc916b0a150f450a94a12458eef42
fadhilZamanNow <fadhilisfadhillah@gmail.com>
2024-05-15 03:37:04 +0700

    selesai ganti versi redux

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import { LoginPage, SignUpPage,ActivationPage } from "./Routes"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from "react";
-import { server } from "./server";
-import axios from "axios";
-import { toast } from "react-toastify";
 
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/user";
 export default function App() {
   useEffect(() => {
-    axios.get(`${server}/user/getuser`,{withCredentials : true}).then((res) => {
-      console.log(res.data);
-      toast.success(res.data.message);
-    }).catch((err) => {
-      toast.error(err.response.data.message)
-    })
+    Store.dispatch(loadUser());
   },[])
 
   return (
